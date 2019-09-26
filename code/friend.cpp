6aad60ea09d85345667e9d8d9a5b04b3a049cd46
Luciano Candal <51853805+Lucinotion@users.noreply.github.com>
2019-09-26 23:57:15 +0200

    Update friend.cpp

--- code/friend.cpp
+++ code/friend.cpp
@@ -1,20 +1,26 @@
 class regularClass{
 
-    private:
+private:
 
     auto getPrivate(){
         return "This fuction is private";
     }
 
-    public:
+public:
 
     auto getPublic(){
         return "This function is public";
     }
 
+    // FRIEND CLASS DECLARATION
     friend class friendClass;
+    
+    // FRIEND CLASS MEMBER DECLARATION
+    friend friendMember::execute(regularClass rc);
+    
 };
 
+// FRIEND CLASS DEFINITION
 class friendClass{
 
     void execute(regularClass rc){
@@ -24,5 +30,9 @@ class friendClass{
 };
 
 class friendMember{
+    // FRIEND CLASS MEMBER DEFINITION
+    auto execute(regularClass rc){
+        rc.getPrivate();
+    }
 
 };
