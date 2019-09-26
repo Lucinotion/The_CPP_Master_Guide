fc516c57c7f89402781c37fcc361608e51c8f69f
Luciano Candal <51853805+Lucinotion@users.noreply.github.com>
2019-09-27 00:01:11 +0200

    Update helloworld.cpp

--- code/helloworld.cpp
+++ code/helloworld.cpp
@@ -1,8 +1,8 @@
-#include <iostream> // This includes the library iostream, which allows te usage of functions such as cout and endl
+#include <iostream> // This includes the library iostream, which allows te usage of functions such as cout and endl.
 
-int main() // The main function is the body for every c++ program
+int main() // The main function is the body for every c++ program.
 {
     std::cout << "Hello world!" << std::endl;
 
-    return 0; // A program returns 0 when it was executed successfully
+    return 0; // A program returns 0 when it was executed successfully.
 }
